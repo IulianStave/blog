0050905455b6476cd33194601027c2481c1f934e
iulianstave <iulian.staverescu@gmail.com>
2019-11-23 18:35:22 +0200

    Filter date format, register posts into admin section

--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,6 @@
 from django.shortcuts import render
 from .models import Post
 
-"""
-posts = [
-    {
-        'author':'CoreyMS',
-        'title':'Blog Post one',
-        'content':'First post content',
-        'date_posted':'August 27,2018'
-    },
-    {
-        'author':'Stave Iulian',
-        'title':'Blog Stave Post',
-        'content':'Second post content',
-        'date_posted':'August 7,2018'
-    }
-]
-"""
 def home(request):
     context = {
         'posts': Post.objects.all(),
