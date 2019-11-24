df59644534cc2511efb8c1f5ad56b74a86ac1e5f
iulianstave <iulian.staverescu@gmail.com>
2019-11-24 12:37:11 +0200

    Add profile page and route to urls

--- users/views.py
+++ users/views.py
@@ -1,13 +1,9 @@
 from .forms import UserRegistrationForm
 from django.shortcuts import render, redirect
 from django.contrib import messages
-
-
-
-# Create your views here.
+from django.contrib.auth.decorators import login_required
 
 def register(request):
-    
     if request.method == 'POST':
         registration_form = UserRegistrationForm(request.POST)
         if registration_form.is_valid():
@@ -17,6 +13,8 @@ def register(request):
             return redirect('login')
     else:
         registration_form = UserRegistrationForm()
-    
-    
     return render(request, 'users/register.html', { 'form': registration_form })
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
