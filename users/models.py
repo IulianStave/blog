1e3f8879fddd824ca5247aa002b8fc744ff9052e
iulianstave <iulian.staverescu@gmail.com>
2019-11-24 14:57:54 +0200

    Add Profile model to add user profile picture

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Extend the existing user model that Django provide to add user picture
+class Profile(models.Model):
+    #associate a user to the profile
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    image = models.ImageField(default = 'default.jpg', upload_to = 'profile_pics')
+
+def __str__(self):
+    return f'{self.user.username} Profile'
