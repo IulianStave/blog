23b0107b6a008d6769a97f940ecb4177bdf00472
iulianstave <iulian.staverescu@gmail.com>
2019-11-25 22:30:24 +0200

    update user model

--- users/models.py
+++ users/models.py
@@ -12,9 +12,11 @@ class Profile(models.Model):
         return f'{self.user.username} Profile'
     
     def save(self, *args, **kwargs):
-        super(Profile.self).save(*args,**kwargs)
+        #super().save(*args,**kwargs)
+        super().save()
 
         img = Image.open(self.image.path)
+        
         if img.height > 300 or img.width > 300:
             output_size = (300, 300)
             img.thumbnail(output_size)
